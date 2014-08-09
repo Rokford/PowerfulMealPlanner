fa57614ddaac4081323ecd20a73905718656f9ad
Rokford <g.izworski@gmail.com>
2014-08-09 19:57:21 +0200

    Added "Szuflad" (Navigation Drawer)

--- app/src/main/java/com/example/powerfulshoppinglist/CalendarActivity.java
+++ app/src/main/java/com/example/powerfulshoppinglist/CalendarActivity.java
@@ -1,20 +1,98 @@
 package com.example.powerfulshoppinglist;
 
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.example.powerfulshoppinglist.R;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.roomorama.caldroid.CaldroidFragment;
+
 import java.util.Calendar;
 
-public class CalendarActivity extends ActionBarActivity {
+public class CalendarActivity extends ActionBarActivity
+{
+    private DrawerLayout drawerLayout;
+
+    private ActionBarDrawerToggle drawerToggle;
+
+    private ListView drawerList;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.navigation_drawer_item, Utilities.navigationItemsArray));
+
+        getSupportActionBar().setTitle(getResources().getString(R.string.shopping_list));
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.yes, R.string.no)
+        {
+            @Override
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView)
+            {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset)
+            {
+
+                super.onDrawerSlide(drawerView, slideOffset);
+            }
+        };
+
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long arg3)
+            {
+                String value = (String) adapter.getItemAtPosition(position);
+                if (value == getResources().getString(R.string.recipies_list))
+                {
+                    Intent intent = new Intent(CalendarActivity.this, RecipeListActivity.class);
+                    startActivity(intent);
+                }
+                else if (value == getResources().getString(R.string.shopping_list))
+                {
+                    Intent intent = new Intent(CalendarActivity.this, ShoppingListActivity.class);
+                    startActivity(intent);
+                }
+                else if (value == getResources().getString(R.string.callendar))
+                {
+                    Intent intent = new Intent(CalendarActivity.this, CalendarActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
 
         CaldroidFragment caldroidFragment = new CaldroidFragment();
         Bundle args = new Bundle();
@@ -30,21 +108,37 @@ public class CalendarActivity extends ActionBarActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.calendar, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == android.R.id.home)
+        {
+            if (drawerLayout.isDrawerOpen(Gravity.LEFT))
+                drawerLayout.closeDrawers();
+            else
+                drawerLayout.openDrawer(Gravity.LEFT);
         }
+        else if (item.getItemId() == R.id.menu_add)
+        {
+            Intent intent = new Intent(this, AddShoppingItemActivity.class);
+            startActivity(intent);
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
     }
 }
